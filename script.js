27ed032fb9fcdc5727a56819c7c40c191d1efdbe
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-15 23:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+let width = 0;
+let height = 0;
+
+const eventHandler = ()=>{
+
+	const ele1 = document.getElementById("s1");
+	const ele2 = document.getElementById("s2");
+	
+	ele1.innerText = window.innerWidth;
+	ele2.innerText = window.innerHeight;
+
+}
+
+const displayContent = ()=>{
+
+	const divElement = document.createElement("div");
+
+	const heading = `<h2> Width: <span id = s1>${window.innerWidth}</span> and Height: <span id = s2>${window.innerHeight}</span></h2>`;
+
+    divElement.innerHTML= heading;
+	document.body.appendChild(divElement);	
+}
+
+displayContent();
+
+
+window.onresize = eventHandler;
